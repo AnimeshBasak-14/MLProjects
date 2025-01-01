05fc11434570b619a0a0e7c77c7fff51ec9d6fa9
Animesh Basak <basakanimesh16@gmail.com>
2025-01-01 22:00:45 +0530

    Fix formatting and improve error handling in CustomException class

--- src/exception.py
+++ src/exception.py
@@ -9,16 +9,16 @@ def err_msg_detail(error, error_detail:sys):
     error_message = "Error occured in python script name [{0}] at line number [{1}] error message [{2}]".format(
         file_name,
         line_number,
-        str(error)   
+        str(error))   
         
-        return error_message     
-    )
+    return error_message     
+    
     
     
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
-        self.error_mess = err_msg_detail(error_message, error_detail=error_detail)
+        super().__init__(error_message)
+        self.error_message = err_msg_detail(error_message, error_detail=error_detail)
         
     def __str__(self):
         return self.error_message
