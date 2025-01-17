e6201aa2ec608ceff4336bcacdbf3375fb3554ae
Animesh Basak <basakanimesh16@gmail.com>
2025-01-17 07:54:50 +0530

    EDA and Probelem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from logger import logging
 
 def err_msg_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -22,3 +22,9 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)
