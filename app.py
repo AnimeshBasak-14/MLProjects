a7b3084d48f2dde7aef5ae7e07092c0a1abe9aae
Animesh Basak <basakanimesh16@gmail.com>
2025-01-17 22:53:11 +0530

    refactor: rename application.py to app.py and update references

--- app.py
+++ app.py
@@ -2,18 +2,21 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
 
+app=application
+
 ## Route for a home page
 
-@application.route('/')
+@app.route('/')
 def index():
     return render_template('index.html') 
 
-@application.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -25,8 +28,7 @@ def predict_datapoint():
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-        )
+            writing_score=float(request.form.get('reading_score')))
         
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
@@ -37,9 +39,8 @@ def predict_datapoint():
     
     
 
-   
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=8080)  
-    # app.run(host="0.0.0.0",debug=True)        
+if __name__=="__main__":
+    # app.run(host="0.0.0.0",port=8080)        
+    app.run(host='0.0.0.0', port=8080)        
 
 
